899e18a70d937548d086adeb82ddcc550f29a443
ssakod <153281411+ssakod@users.noreply.github.com>
2025-01-31 14:07:54 +0500

    asdw2eqнмрииро

---
+++ Biblioteka/wwwroot/lib/bootstrap/dist/js/site.js
@@ -0,0 +1,10 @@
+﻿window.openFileDialog = () => {
+    return new Promise((resolve, reject) => {
+        const input = document.createElement("input");
+        input.type = "file";
+        input.onchange = (event) => {
+            resolve(event.target.files[0].name);
+        };
+        input.click();
+    });
+};
