20331a7aff8e4ae9bb89f02e6f666536d66946d3
ssakod <153281411+ssakod@users.noreply.github.com>
2025-01-31 12:48:27 +0500

    Add project files.

---
+++ Biblioteka/Components/Layout/_Host.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Biblioteka.Components.Layout
+{
+    public class _HostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
